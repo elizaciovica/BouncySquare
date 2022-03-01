a92cf4396129777f3c8b28e6ff0e10fb9c5e5859
Eliza <elizaciovica0208@gmail.com>
2022-03-01 19:42:36 +0200

    all the other changes -Replace the first number in the vertices array with -2.0, instead of -1.0. -Replace the color of the bouncy square with green, and then with blue. -In the glDrawElements call, use the other symbolic constants GL_POINTS, GL_LINE_STRIP,  GL_LINE_LOOP, GL_LINES, GL_TRIANGLE_STRIP, GL_TRIANGLE_FAN.  -Increase the value of mTransY with increments of 0.3.  - Leave the dithering enabled.  - Change the color (and alpha value) of the background.

--- app/src/main/java/cg/bouncysquare/Square.java
+++ app/src/main/java/cg/bouncysquare/Square.java
@@ -16,7 +16,7 @@ public class Square {
     public Square() {
         float vertices[] =
                 {
-                        -1.0f, -1.0f,
+                        -2.0f, -1.0f,
                         1.0f, -1.0f,
                         -1.0f, 1.0f,
                         1.0f, 1.0f
@@ -25,10 +25,27 @@ public class Square {
         byte maxColor=(byte)255;
         byte colors[] =
                 {
+                        // initial red color
+                        /*
                         0, 0, 0, maxColor,
                         maxColor, 0, 0, maxColor,
                         0, 0, 0, maxColor,
                         maxColor, 0, 0, maxColor,
+                         */
+
+                        // green color
+                       /*
+                        0, maxColor, 0, maxColor,
+                        0, maxColor, 0, maxColor,
+                        0, maxColor, 0, maxColor,
+                        0, maxColor, 0, maxColor,
+                        */
+
+                        // blue color
+                        0, 0, maxColor, maxColor,
+                        0, 0, maxColor, maxColor,
+                        0, 0, maxColor, maxColor,
+                        0, 0, maxColor, maxColor,
                 };
 
         byte indices[] =
@@ -58,7 +75,14 @@ public class Square {
 
         gl.glVertexPointer(2, GL11.GL_FLOAT, 0, mFVertexBuffer);
         gl.glColorPointer(4, GL11.GL_UNSIGNED_BYTE, 0, mColorBuffer);
-        gl.glDrawElements(GL11.GL_TRIANGLES, 6, GL11.GL_UNSIGNED_BYTE, mIndexBuffer);
+        //gl.glDrawElements(GL11.GL_TRIANGLES, 6, GL11.GL_UNSIGNED_BYTE, mIndexBuffer);
+        //gl.glDrawElements(GL11.GL_POINTS, 6, GL11.GL_UNSIGNED_BYTE, mIndexBuffer);
+        //gl.glDrawElements(GL11.GL_LINE_STRIP, 6, GL11.GL_UNSIGNED_BYTE, mIndexBuffer);
+        //gl.glDrawElements(GL11.GL_LINE_LOOP, 6, GL11.GL_UNSIGNED_BYTE, mIndexBuffer);
+        //gl.glDrawElements(GL11.GL_LINES, 6, GL11.GL_UNSIGNED_BYTE, mIndexBuffer);
+        gl.glDrawElements(GL11.GL_TRIANGLE_STRIP, 6, GL11.GL_UNSIGNED_BYTE, mIndexBuffer);
+        //gl.glDrawElements(GL11.GL_TRIANGLE_FAN, 6, GL11.GL_UNSIGNED_BYTE, mIndexBuffer);
+
         gl.glFrontFace(GL11.GL_CW);
     }
 }
--- app/src/main/java/cg/bouncysquare/SquareRenderer.java
+++ app/src/main/java/cg/bouncysquare/SquareRenderer.java
@@ -14,18 +14,21 @@ public class SquareRenderer implements  GLSurfaceView.Renderer {
         mSquare = new Square();
     }
 
+    @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 
-        gl.glDisable(GL10.GL_DITHER);
+        //gl.glDisable(GL10.GL_DITHER);
+        gl.glEnable(GL10.GL_DITHER);
         gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
 
-        gl.glClearColor(0,0,0,0);
+        gl.glClearColor(1,1,0,1);
         gl.glEnable(GL10.GL_CULL_FACE);
         gl.glShadeModel(GL10.GL_SMOOTH);
         gl.glEnable(GL10.GL_DEPTH_TEST);
 
     }
 
+    @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
 
         gl.glViewport(0, 0, width, height);
@@ -38,6 +41,7 @@ public class SquareRenderer implements  GLSurfaceView.Renderer {
 
     }
 
+    @Override
     public void onDrawFrame(GL10 gl) {
 
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
@@ -46,7 +50,8 @@ public class SquareRenderer implements  GLSurfaceView.Renderer {
         gl.glLoadIdentity();
 
         gl.glTranslatef(0.0f,(float)Math.sin(mTransY), -3.0f);
-        mTransY += 0.075f;
+        //mTransY += 0.075f;
+        mTransY += 0.3f;
 
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
         gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
